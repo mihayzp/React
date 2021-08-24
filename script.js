d23f5f11c2b76febdbeb19b7ce50c3167e0cfc68
Michael <mikhail.tkalich@gmail.com>
2021-08-24 14:30:01 +0300

    homework Number

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 let numberOfFilms;
-prompt numberOfFilms:("How much movies have you seen?", '' );
+numberOfFilms = prompt ('How much movies have you seen?', '' );
 console.log(numberOfFilms);
+
+let personalMovieDB = {
+    count: +numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+
+
+};
+
+let a = prompt('один из последниз фильмов',''),
+    b = prompt('Оценка',''),
+    c = prompt('один из последниз фильмов',''),
+    d = prompt('Оценка','');
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
