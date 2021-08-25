f1ca27570a9d1d47494bbce082dd853220ffdc78
Michael <mikhail.tkalich@gmail.com>
2021-08-25 09:02:58 +0300

    Homework 24.05'Cycles'

---
+++ function.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// function showFirstMessage() { 
+//     console.log('Hello');
+// }
+// showFirstMessage();
+
+// function calc(a,b) {
+//     return (a + b);
+// }
+// const a = calc(1,2);
+// console.log(a);
+
+// let logger = function() {
+// console.log("hello");
+
+// };
+// logger();
+
+// const calc = (a,b) => a + b;
+
+
+let str = 'test';
+str = str.toLocaleUpperCase()
+console.log(str);
